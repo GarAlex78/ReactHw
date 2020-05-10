f778ec9b1cde060613074061f5369d5414fcb950
GarAlex78 <57135333+GarAlex78@users.noreply.github.com>
2020-05-11 00:36:09 +0300

    add hw2

---
+++ hw2/precache-manifest.cc0aee033dfdec73641b863fc3b4ec46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c655466f89710fb48fa5ddd2e956bac",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "dc03ff4217eb7c3d4cac",
+    "url": "/test/static/css/main.102142eb.chunk.css"
+  },
+  {
+    "revision": "0cd20ca9237e595d89aa",
+    "url": "/test/static/js/2.ca8b0a99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.ca8b0a99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc03ff4217eb7c3d4cac",
+    "url": "/test/static/js/main.52ae3cef.chunk.js"
+  },
+  {
+    "revision": "639cd0bba4ba80a306b9",
+    "url": "/test/static/js/runtime-main.4dbe08c4.js"
+  }
+]);
