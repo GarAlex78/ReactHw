5853188136a3b8853298860a801638e4720d2580
GarAlex78 <57135333+GarAlex78@users.noreply.github.com>
2020-06-01 18:52:44 +0300

    rename

---
+++ hw1_4/precache-manifest.3c4febe3b6be6ded5bc7ef39ef964424.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31fd75dacd9528676663d53d55eab62d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e49174635ac54b7dfc8",
+    "url": "/static/css/main.c2d39c8e.chunk.css"
+  },
+  {
+    "revision": "ca358775aa74aaad2010",
+    "url": "/static/js/2.943b7cb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.943b7cb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e49174635ac54b7dfc8",
+    "url": "/static/js/main.5f42d03c.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
